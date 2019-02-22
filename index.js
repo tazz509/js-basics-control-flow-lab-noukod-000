13da5e53da4c700693b7220c8c7dfed964a4e0f8
carly <carly@noukod.com>
2019-02-22 16:37:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 // Write your code in this file!
-function scuberGreetingForFeet(someValue) {
-  //this is where we can use conditionals given our argument, someValue
-  //don't forget to return whatever the result is!
+function scuberGreetingForFeet(distance)
+{
+
+  if (distance <= 400) {
+    return "This one is on me!"
+  }
+  else if (distance > 2000 &&  distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (distance>2500) {
+    return 'No can do.'
+  }
+}
+
+
+function ternaryCheckCity(city)
+{
+return (city =='NYC' ? 'Ok, sounds good.' : 'No go.');
+}
+
+
+function switchOnCharmFromTip(tip)
+{
+
+switch (tip) {
+  case 'generous':
+ return 'Thank you so much.'
+    break;
+    case 'not as generous':
+   return 'Thank you.'
+      break;
+  default:
+  return 'Bye.'
+
+}
+
+
 }
