76ed210c0f4548b7e3c23827a41b8b78a5259ec9
carly <carly@noukod.com>
2019-02-22 16:36:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  //this is where we can use conditionals given our argument, someValue
+  //don't forget to return whatever the result is!
+}
